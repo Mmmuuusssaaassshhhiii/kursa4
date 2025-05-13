06d23d972f2797365997963cee07c8456152d262
Mmmuuusssaaassshhhiii <alexeibutyaikin04@gmail.com>
2025-05-13 21:33:13 +0300

    laptop controller done

---
+++ kursa4/Controllers/LaptopsController.cs
@@ -0,0 +1,38 @@
+using kursa4.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace kursa4.Controllers;
+
+public class LaptopsController : Controller
+{
+    private readonly IAllLaptops _allLaptops;
+    private readonly ILaptopsCategory _laptopsCategory;
+    private readonly ILaptopsBrand _laptopsBrand;
+    private readonly ILaptopsCpu _laptopsCpu;
+    private readonly ILaptopsGpu _laptopsGpu;
+    private readonly ILaptopsRam _laptopsRam;
+    private readonly ILaptopsStorage _laptopsStorage;
+
+    public LaptopsController(IAllLaptops iAllLaptops,
+        ILaptopsCategory iLaptopsCategory,
+        ILaptopsBrand iLaptopsBrand,
+        ILaptopsCpu iLaptopCpu,
+        ILaptopsGpu iLaptopsGpu,
+        ILaptopsRam iLaptopsRam,
+        ILaptopsStorage iLaptopsStorage)
+    {
+        _allLaptops = iAllLaptops;
+        _laptopsCategory = iLaptopsCategory;
+        _laptopsBrand = iLaptopsBrand;
+        _laptopsCpu = iLaptopCpu;
+        _laptopsGpu = iLaptopsGpu;
+        _laptopsRam = iLaptopsRam;
+        _laptopsStorage = iLaptopsStorage;
+    }
+
+    public ViewResult ListLaptops()
+    {
+        var laptops = _allLaptops.Laptops;
+        return View(laptops);
+    }
+}
