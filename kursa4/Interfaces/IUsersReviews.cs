1f348a05dde93740d1267e3e8d1c6e100b3eefe0
Mmmuuusssaaassshhhiii <alexeibutyaikin04@gmail.com>
2025-05-16 12:24:57 +0300

    review interface

---
+++ kursa4/Interfaces/IUsersReviews.cs
@@ -0,0 +1,21 @@
+using kursa4.Models;
+
+namespace kursa4.Interfaces;
+
+public interface IUsersReviews
+{
+    
+    //get reviews
+    IEnumerable<Review> GetReviewsByLaptopId(int laptopId);
+    IEnumerable<Review> GetReviewsByUserId(int userId);
+    Review GetReviewById(int reviewId);
+    
+    //cre, ed, del
+    void AddReview(Review review);
+    void UpdateReview(Review review);
+    void DeleteReview(int reviewId);
+    
+    //average rating
+    double GetAverageRatingForLaptop(int laptopId);
+    
+}
