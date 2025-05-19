8c4dc93b9d7050f7a515b385fb6fbf03195f5290
Mmmuuusssaaassshhhiii <alexeibutyaikin04@gmail.com>
2025-05-19 15:05:03 +0300

    users interface update

--- kursa4/Interfaces/IAllUsers.cs
+++ kursa4/Interfaces/IAllUsers.cs
@@ -7,7 +7,13 @@ public interface IAllUsers
     IEnumerable<User> Users { get; }
     User GetUser(int userId);
     User GetUserByEmail(string email);
+    
     void AddUser(User user);
     void UpdateUser(User user);
     void DeleteUser(int userId);
+
+    // Дополнительные методы:
+    bool Register(string email, string password, string fullName, string phoneNumber);
+    User Login(string email, string password);
+    void Logout(int userId); // если ведёшь учёт сессий
 }
