429887fa16fabec8fc1c79ccdfc4b2f2c96727cc
Mmmuuusssaaassshhhiii <alexeibutyaikin04@gmail.com>
2025-05-20 15:23:42 +0300

    user model change

--- kursa4/Models/User.cs
+++ kursa4/Models/User.cs
@@ -3,13 +3,21 @@ namespace kursa4.Models;
 public class User
 {
     public int Id { get; set; }
-    public string Email { get; set; }
-    public string Password { get; set; }
-    public string FullName { get; set; }
-    public string PhoneNumber { get; set; }
-    public string Role { get; set; }
-    
-    public Cart Cart { get; set; }
+
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string FullName { get; set; } = string.Empty;
+    public string PhoneNumber { get; set; } = string.Empty;
+    public string Role { get; set; } = "user"; // например, по умолчанию
+
+    public Cart? Cart { get; set; }
+
     public ICollection<Order> Orders { get; set; }
     public ICollection<Review> Reviews { get; set; }
+
+    public User()
+    {
+        Orders = new List<Order>();
+        Reviews = new List<Review>();
+    }
 }
