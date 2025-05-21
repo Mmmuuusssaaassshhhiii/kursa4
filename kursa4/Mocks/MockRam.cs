b81d241e805c3553777e773417b85bdb1d4d23ee
Mmmuuusssaaassshhhiii <alexeibutyaikin04@gmail.com>
2025-05-21 16:23:25 +0300

    mock ram update, adding functions add, del and update

--- kursa4/Mocks/MockRam.cs
+++ kursa4/Mocks/MockRam.cs
@@ -5,15 +5,36 @@ namespace kursa4.Mocks;
 
 public class MockRam : ILaptopsRam
 {
-    public IEnumerable<RAM> AllRams
+    private readonly ApplicationDbContext _context;
+
+    public MockRam(ApplicationDbContext context)
     {
-        get
+        _context = context;
+    }
+
+    public IEnumerable<RAM> AllRams => _context.RAMs.ToList();
+
+    public RAM GetRAM(int id) => _context.RAMs.Find(id);
+
+    public void AddRAM(RAM ram)
+    {
+        _context.RAMs.Add(ram);
+        _context.SaveChanges();
+    }
+
+    public void UpdateRAM(RAM ram)
+    {
+        _context.RAMs.Update(ram);
+        _context.SaveChanges();
+    }
+
+    public void DeleteRAM(int id)
+    {
+        var ram = _context.RAMs.Find(id);
+        if (ram != null)
         {
-            return new List<RAM>
-            {
-                new RAM { Type = "DDR5", SizeGb = 32 },
-                new RAM { Type = "DDR4", SizeGb = 16 },
-            };
+            _context.RAMs.Remove(ram);
+            _context.SaveChanges();
         }
     }
 }
