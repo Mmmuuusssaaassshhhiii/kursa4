905561be45240a5088a176ecb370ebe0a3c87cf5
Mmmuuusssaaassshhhiii <alexeibutyaikin04@gmail.com>
2025-05-21 15:59:51 +0300

    mock brand update, adding functions add, del and update

--- kursa4/Mocks/MockBrand.cs
+++ kursa4/Mocks/MockBrand.cs
@@ -7,10 +7,34 @@ public class MockBrand : ILaptopsBrand
 {
     private readonly ApplicationDbContext _context;
 
-    public MockBrand(ApplicationDbContext context)
+    private MockBrand(ApplicationDbContext context)
     {
         _context = context;
     }
-
+    
     public IEnumerable<Brand> allBrands => _context.Brands.ToList();
+    
+    public Brand GetBrand(int id) => _context.Brands.Find(id);
+
+    public void AddBrand(Brand brand)
+    {
+        _context.Brands.Add(brand);
+        _context.SaveChanges();
+    }
+
+    public void UpdateBrand(Brand brand)
+    {
+        _context.Brands.Update(brand);
+        _context.SaveChanges();
+    }
+
+    public void DeleteBrand(int id)
+    {
+        var brand = _context.Brands.Find(id);
+        if (brand != null) 
+        {
+            _context.Brands.Remove(brand);
+            _context.SaveChanges();
+        }
+    }
 }
