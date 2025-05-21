379c44c21b116bfcd226a803be94837fc6cb12bf
Mmmuuusssaaassshhhiii <alexeibutyaikin04@gmail.com>
2025-05-21 15:12:38 +0300

    admin controller

---
+++ kursa4/Controllers/AdminController.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace kursa4.Controllers;
+
+public class AdminController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public AdminController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var users = await _context.Users.ToListAsync();
+        var orders = await _context.Orders.ToListAsync();
+        var laptops = await _context.Laptops.ToListAsync();
+        
+        ViewBag.UserCount = users.Count;
+        ViewBag.OrderCount = orders.Count;
+        ViewBag.LaptopCount = laptops.Count;
+
+        return View();
+    }
+
+    public async Task<IActionResult> Laptops()
+    {
+        var laptops = await _context.Laptops
+            .Include(l => l.Category)
+            .Include(l => l.Brand)
+            .Include(l => l.CPU)
+            .Include(l => l.GPU)
+            .Include(l => l.RAM)
+            .Include(l => l.Storage)
+            .ToListAsync();
+        
+        return View(laptops);
+    }
+
+    public async Task<IActionResult> Users()
+    {
+        var users = await _context.Users.ToListAsync();
+
+        return View(users);
+    }
+
+    public async Task<IActionResult> Orders()
+    {
+        var orders = await _context.Orders
+            .Include(o => o.User)
+            .ToListAsync();
+        
+        return View(orders);
+    }
+}
--- kursa4/obj/Debug/net8.0/kursa4.AssemblyInfo.cs
+++ kursa4/obj/Debug/net8.0/kursa4.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("kursa4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1e5225258b7408d08ca1a1b13bb01d65cfe42ea3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f9fdd047a2a740369da905de0a1cbc85e5ab518")]
 [assembly: System.Reflection.AssemblyProductAttribute("kursa4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("kursa4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
